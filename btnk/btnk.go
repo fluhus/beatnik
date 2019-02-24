239a6cb3754f99f8c2b64ea8ca9189a8011961cf
fluhus <doctor_troll@walla.co.il>
2019-02-24 20:48:58 +0200

    add bad exit code to btnk in case of parse error

--- btnk/btnk.go
+++ btnk/btnk.go
@@ -10,27 +10,33 @@ import (
 )
 
 func main() {
+	exitCode := 0
 	for _, f := range os.Args[1:] {
 		fmt.Printf("reading %q\n", f)
 		d, err := ioutil.ReadFile(f)
 		if err != nil {
 			fmt.Printf("failed to read %q: %v\n", f, err)
+			exitCode = 2
 			continue
 		}
 		t, err := beatnik.ParseTrack(string(d))
 		if err != nil {
 			fmt.Printf("failed to parse %q: %v\n", f, err)
+			exitCode = 2
 			continue
 		}
 		b, err := t.MarshalBinary()
 		if err != nil {
 			fmt.Printf("failed to encode: %v\n", err)
+			exitCode = 2
 			continue
 		}
 		err = ioutil.WriteFile(f+".mid", b, 0600)
 		if err != nil {
 			fmt.Printf("failed to write %q: %v\n", f+".mid", err)
+			exitCode = 2
 			continue
 		}
 	}
+	os.Exit(exitCode)
 }
